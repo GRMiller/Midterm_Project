8616d1dff4b163cba30309d354a48c2b582ab3c0
Garrett Miller <garrettralphmiller@gmail.com>
2016-11-11 22:42:41 -0500

    Seats being pushed into Array as objects with seatNumber properties

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 
-//FOR LOOP
+//MAKE SEATING CHART
 for (var i = 1; i <= 24; i++) {
    var seatChart = document.getElementById('seating_chart');
    var newDiv = document.createElement('div');
@@ -8,48 +8,89 @@ for (var i = 1; i <= 24; i++) {
    seatChart.appendChild(newDiv);
 }
 
-/*
-
 
 // MAKE ARRAY TO STORE RESERVED SEATS
 
 var reservedSeats = [];
+var seatNumber = []
+
+
+
+
 
 //USE CONSTRUCTOR TO MAKE OBJECT FOR reservedSeats 
-var Seat = function (name, seatNumber, email) {		//DECLARE CONSTRUCTOR OBJECT
 
-//SPECIFY THAT EACH TIME A NEW OBJECT IS CREATED THE CURRENT PARAMETERS WILL BE THE PARAMETERS OF THE NEW OBJECT
-	this.name = name;										
+var Seat = function (seatNumber) {		//DECLARE CONSTRUCTOR OBJECT
+
+	// this.name = name;										
 	this.seatNumber = seatNumber;
-	this.price = Number(price); //USE OUR SHOPPING CART  SCRIPTS TO ADD TOTAL
-	this.qty = seatNumber.length;
-}
+	// this.price = Number(price); //USE OUR SHOPPING CART  SCRIPTS TO ADD TOTAL
+	// this.qty = seatNumber.length;
+};
+
+
+
+
+
+//MAKE FUNCTION THAT LETS USER CHOOSE SEAT. 
+	//IF SEAT IS AVAILABLE GENERATE FORM FOR USER INPUT
+	//IF SEAT IS UNAVAILABLE PROMPT MESSAGE SEAT TAKEN
+
+$('.chair').on('click', function (){
+	
+	var seatNumber = $(this).attr('id');
+
+	if ($('available')) {              //maybe ($(this).hasClass('available'))
+		reservedSeats.push (new Seat (seatNumber));
+		// $(this).css(backgroundColor, "#2e4629")
+	} else if ($('taken')) {
+		prompt("This seat is already taken. Please choose another seat")
+	};
+});
+
+
+
 
 
 //reserveSeat ()
 	//ON CLICK OF "seat open" PUSH RESERVED SEATS INTO ARRAY
+
+var email;
 function reserveSeat(name, seatNumber, email) {
 	
-	var name = getElementById("Name").text;
-	var email = getElementById("Email").text;
-	if(reservedSeats[i].name === name) {
-		prompt("We're sorry, this seat is already taken");
-	}
+	var name = document.getElementById("Name").text;
+	var email = document.getElementById("Email").text;
+	
+	for (var i = 0; i < reservedSeats.length; i++) { //LOOP THROUGH PROPERTIES OF OBJECTS IN CART
+		
+		// if (reservedSeats[i].seatNumber === seatNumber) {	//TO CHECK FOR DUPLICATE ITEMS
+			reservedSeats[i].name = this.name;			//ADD NAME TO OBJECT
+			reservedSeats[i].email = this.email;	
+			
+			return; //STOP LOOP AND ACT
+		}	//END FUNCTION when IF = TRUE
 
-	reservedSeats.push(new Seat (name, seatNumber, email));
-};
+	}	//END LOOP
+; //END FUNCTION
 
-$('reserveBtn')
-
+$('reserveBtn').on('click', reserveSeat(name, seatNumber, email)) 
 //listReservedSeats()
 	//ON CLICK OF "reserveBtn" DISPLAY RESERVED SEATS & FORM for USER INPUT
 		//This is where we could include totalPrice from shopping cart
 
 
 
+
+
 //displayConfirmation ()
 	//ON CLICK OF "submitBtn" DISPLAY CONFIRMATION
 
+
+//removeReservedSeat ()
+
+function removeReservedSeatAll () {
+	var reservedSeats = [];
+};
 
 
 
@@ -58,7 +99,7 @@ $('reserveBtn')
 
 //WHEN HOVERING OVER SEAT IN reservedSeats array display Person's Name
 
-*/
+
 
 
 
