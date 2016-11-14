8b000f29f40af8096da841737c719f5b0ab6c63c
Garrett Miller <garrettralphmiller@gmail.com>
2016-11-14 13:05:16 -0500

    Updates - not working

--- JS/scripts.js
+++ JS/scripts.js
@@ -12,16 +12,16 @@ for (var i = 1; i <= 4; i++) {
 }
 
 // ROW 2
-for (var i = 1; i <= 6; i++) {
+for (var i = 5; i <= 10; i++) {
    var seatChart = document.getElementById('seating_chart1');
    var newDiv = document.createElement('div');
    newDiv.id = 'seat' + i;
-   newDiv.className = "chair available";
+   newDiv.className = "chair available ";
    seatChart.appendChild(newDiv);
 }
 
 // ROW 3
-for (var i = 1; i <= 6; i++) {
+for (var i = 11; i <= 16; i++) {
    var seatChart = document.getElementById('seating_chart2');
    var newDiv = document.createElement('div');
    newDiv.id = 'seat' + i;
@@ -30,7 +30,7 @@ for (var i = 1; i <= 6; i++) {
 }
 
 // ROW 4
-for (var i = 1; i <= 8; i++) {
+for (var i = 17; i <= 24; i++) {
    var seatChart = document.getElementById('seating_chart3');
    var newDiv = document.createElement('div');
    newDiv.id = 'seat' + i;
@@ -38,15 +38,15 @@ for (var i = 1; i <= 8; i++) {
    seatChart.appendChild(newDiv);
 }
 
-
 // END SEATING CHART LOOP
 
 
 
-// MAKE ARRAY TO STORE RESERVED SEATS
 
+
+// DECLARE VARIABLES 
 var reservedSeats = [];
-var seatNumber = []
+var seatNumber = [];
 var email;
 var name;
 var seatNumber;
@@ -54,9 +54,10 @@ var seatNumber;
 
 
 
-//USE CONSTRUCTOR TO MAKE OBJECT FOR reservedSeats 
 
-var Seat = function (seatNumber) {		//DECLARE CONSTRUCTOR OBJECT
+
+//CONSTRUCTOR OBJECT
+var Seat = function (name, seatNumber, email) {		//DECLARE CONSTRUCTOR OBJECT
 
 	this.name = name;										
 	this.seatNumber = seatNumber;
@@ -67,42 +68,42 @@ var Seat = function (seatNumber) {		//DECLARE CONSTRUCTOR OBJECT
 
 
 
-//MAKE FUNCTION THAT LETS USER CHOOSE SEAT. 
-	//IF SEAT IS AVAILABLE CHANGE CLASS TO TAKEN
-	//IF SEAT IS UNAVAILABLE PROMPT MESSAGE SEAT TAKEN
 
+//CHANGE CLASS IF AVAILABLE OR TELL USER SEAT IS UNAVAILABLE 
 $('.available').on('click', function (){
-	
-	var seatNumber = $(this).attr('id');
 
 	$(this).removeClass('available').addClass('taken');
-
-	
  
  	for (var i = 0; i < reservedSeats.length; i++) {
  		if (reservedSeats[i].seatNumber === seatNumber) {
  			alert("This seat is already taken. Please choose another seat.");
  			return;
  		}
- 		
  	}
-	
-	reservedSeats.push (new Seat (seatNumber));
+	reservedSeats.push (new Seat (seatNumber)); // Maybe wait to make object tell all info collected
 });
 
+// $('.taken').on('click', function () {
+
+// 	alert("This seat is already taken. Please choose another.")
+
+// });
 
 
 
 
-// on click of "reserveBtn" Generate form for user input and display seats with "taken class"
-	$('.reserveBtn').on('click', function () { 	//choose selector and event to trigger event
+//CLICK RESERVE BUTTON
+	//Form with user's selected seats and input fields for name and email appears
 
-		for (var i = 0; i < reservedSeats.length; i++) {
- 			var elSelectedSeats = document.getElementById('selected_seats');
- 			elSelectedSeats.appendChild(reservedSeats[i].seatNumber)
+// 	$('.reserveBtn').on('click', function () { 	//choose selector and event to trigger event
 
-		}
-	});
+// 		for (var i = 0; i < reservedSeats.length; i++) {
+//  			var elSelectedSeats = document.getElementById('selected_seats');
+//  			v
+//  			elSelectedSeats.appendChild(reservedSeats[i].seatNumber)
+
+// 		}
+// 	});
 		// for (var i = 0; i < reservedSeats.length; i++) {
 		// reservedSeats.forEach(function () {
 
@@ -123,27 +124,20 @@ $('.available').on('click', function (){
 
 
 
-//reserveSeat ()
-	//ON CLICK OF "submitBtn" PUSH RESERVED SEATS INTO ARRAY
+//reserveSeat() on CLICK SUBMIT
 
-// var email;
-// function reserveSeat(name, seatNumber, email) {
+function reserveSeat(name, seatNumber, email) {
 	
-// 	var name = document.getElementById("Name").text;
-// 	var email = document.getElementById("Email").text;
+	var name = 'john';
+	var email = 'john@john.com';
+	var seatNumber = $('.taken').attr('id');
 	
-// 	for (var i = 0; i < reservedSeats.length; i++) { //LOOP THROUGH PROPERTIES OF OBJECTS IN CART
-		
-// 		{	//TO CHECK FOR DUPLICATE ITEMS
-// 			reservedSeats[i].name = this.name;			//ADD NAME TO OBJECT
-// 			reservedSeats[i].email = this.email;	
-			
-// 			return; //STOP LOOP AND ACT
-// 		}	//END FUNCTION when IF = TRUE
+	reservedSeats.push (new Seat (name, seatNumber, email));
+	
+};
 
-// 	}	//END LOOP
-//  //END FUNCTION
-// };
+$('.submitBtn').on('click', reserveSeat(name, seatNumber, email));
+
 
 
 
@@ -167,9 +161,30 @@ $('.available').on('click', function (){
 
 
 
-//BONUS
+//BONUS - 
+/*2 ideas: 	1)inside of the reserveSeat function use a setAttribute method 
+			To add attribute of title to images 
+			Title value is user's information 
+			On hover that info will automatically display
 
-//WHEN HOVERING OVER SEAT IN reservedSeats array display Person's Name
+			2)On click of taken div Alert will pop up this seat is reserved by:
+			Function with for each loop runs and finds matching seat number
+			Returns property value of name*/
+
+
+// $('.taken').on('click', function (){
+
+// 	 $(this).;
+ 
+//  	for (var i = 0; i < reservedSeats.length; i++) {
+//  		if (reservedSeats[i].seatNumber === seatNumber) {
+//  			alert("This seat is already taken. Please choose another seat.");
+//  			return;
+//  		}
+//  	}
+// 	// reservedSeats.push (new Seat (seatNumber)); // Maybe wait to make object tell all info collected
+// });
+
 
 
 
